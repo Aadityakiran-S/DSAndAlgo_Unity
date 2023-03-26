5088eca66bcfd5e50b0322f54737c01721a83359
AadityakiranS <aadityakirans@gmail.com>
2023-03-26 12:35:13 +0530

    Added some comments to the iterative swap connections part of "Swap Pairs of Nodes in Linked List"

--- DSAndAlgo/Assets/DataStructuresAndAlgorithms/Scripts/Problems/Medium/LinkedLists/SwapPairsOfNodes.cs
+++ DSAndAlgo/Assets/DataStructuresAndAlgorithms/Scripts/Problems/Medium/LinkedLists/SwapPairsOfNodes.cs
@@ -5,154 +5,162 @@ using System.Collections.Generic;
 
 public class SwapPairsOfNodes : MonoBehaviour
 {
-	#region Question
-	
-	
+    #region Question
 
-	#endregion
 
-	#region References
-	
-	
-	
-	#endregion
-	
-	#region UnityLifecycle
-	
-	//Use this to initialize
-	private void Awake()
-	{
-	
-	}	
-	
-	//Use this to run
-	private void Start()
+
+    #endregion
+
+    #region References
+
+
+
+    #endregion
+
+    #region UnityLifecycle
+
+    //Use this to initialize
+    private void Awake()
     {
-        
+
     }
 
-	#endregion
+    //Use this to run
+    private void Start()
+    {
 
-	#region Methods
+    }
 
-	public ListNode SwapPairs_Iterative(ListNode head)
-	{
-		if (head == null)
-		{
-			return null;
-		}
-		else if (head.next == null)
-		{
-			return head;
-		}
+    #endregion
 
-		ListNode curr = head; ListNode toReturn = curr.next;
-		while (curr != null && curr.next != null)
-		{
-			ListNode next = curr.next;
-			ListNode next2 = next.next;
-			next.next = curr;
+    #region Methods
 
-			if (next2 == null)
-			{
-				curr.next = null;
-				break;
-			}
-			else
-			{
-				ListNode next3 = next2.next;
+    public ListNode SwapPairs_Iterative(ListNode head)
+    {
+        //Base cases: Simply returning the expected value   
+        if (head == null)
+        {
+            return null;
+        }
+        else if (head.next == null)
+        {
+            return head;
+        }
 
-				if (next3 == null)
-				{
-					curr.next = next2;
-					break;
-				}
-				else
-				{
-					curr.next = next3;
-					curr = next2;
-				}
-			}
-		}
+        //Basically we need to swap two entries and put the first entry's next as either the 3rd one or the 4th one, 
+        //depending on which of those exists
+        ListNode curr = head; ListNode toReturn = curr.next;
+        while (curr != null && curr.next != null)
+        {
 
-		return toReturn;
-	}
+            //first and second are guarenteed to exist
+            ListNode next = curr.next;
+            ListNode next2 = next.next;
+            next.next = curr;
 
-	public ListNode SwapPairs_Iterative_Values(ListNode head)
-	{
-		ListNode curr = head;
+            //Checking if third exists
+            if (next2 == null)
+            {
+                curr.next = null;
+                break;
+            }
+            else
+            {
+                ListNode next3 = next2.next;
 
-		while (curr != null && curr.next != null)
-		{
-			SwapValues(curr, curr.next);
+                //Third exists but fourth doesn't => Simply connect first to third and return
+                if (next3 == null)
+                {
+                    curr.next = next2;
+                    break;
+                }
+                else
+                {
+                    //Fourth exists? Then connect first to fourth and repeat process starting from third.
+                    curr.next = next3;
+                    curr = next2;
+                }
+            }
+        }
 
-			curr = curr.next.next;
-		}
+        return toReturn;
+    }
 
-		return head;
-	}
+    public ListNode SwapPairs_Iterative_Values(ListNode head)
+    {
+        ListNode curr = head;
 
-	public ListNode SwapPairs_Values_Recursive(ListNode head)
-	{
-		SwapPairs_Values_Recursive_Internal(head);
-		return head;
-	}	
+        while (curr != null && curr.next != null)
+        {
+            SwapValues(curr, curr.next);
 
-	public ListNode SwapPairs_Recursive(ListNode head)
-	{
-		return SwapPairs_Recursive_Internal(head);
-	}
+            curr = curr.next.next;
+        }
 
-	#endregion
+        return head;
+    }
 
-	#region Helper Methods	
+    public ListNode SwapPairs_Values_Recursive(ListNode head)
+    {
+        SwapPairs_Values_Recursive_Internal(head);
+        return head;
+    }
 
-	private ListNode SwapPairs_Recursive_Internal(ListNode curr)
-	{
-		if (curr == null)
-		{
-			return null;
-		}
+    public ListNode SwapPairs_Recursive(ListNode head)
+    {
+        return SwapPairs_Recursive_Internal(head);
+    }
 
-		//The next of the current pair depends on which is the next2Next in the next pair
-		ListNode next = curr.next; ListNode next2Next = null;
-		
-		//At the end we'll get next as null so we need to be careful
-		if (next != null)
-		{
-			next2Next = next.next; next.next = curr;
-		}
+    #endregion
 
-		curr.next = SwapPairs_Recursive_Internal(next2Next);
+    #region Helper Methods	
 
-		//If next is null, return current otherwise return null. 
-		//This is to prevent prevent last node from being dropped from odd notes
-		return next ?? curr; 
-	}
+    private ListNode SwapPairs_Recursive_Internal(ListNode curr)
+    {
+        if (curr == null)
+        {
+            return null;
+        }
 
-	private void SwapPairs_Values_Recursive_Internal(ListNode curr)
-	{
-		if (curr == null)
-		{
-			return;
-		}
+        //The next of the current pair depends on which is the next2Next in the next pair
+        ListNode next = curr.next; ListNode next2Next = null;
 
-		ListNode next = curr.next;
+        //At the end we'll get next as null so we need to be careful
+        if (next != null)
+        {
+            next2Next = next.next; next.next = curr;
+        }
 
-		if (next == null)
-		{
-			return;
-		}
+        curr.next = SwapPairs_Recursive_Internal(next2Next);
 
-		SwapValues(curr, next);
-		SwapPairs_Values_Recursive_Internal(next.next);
-	}
+        //If next is null, return current otherwise return null. 
+        //This is to prevent prevent last node from being dropped from odd notes
+        return next ?? curr;
+    }
 
-	private void SwapValues(ListNode one, ListNode two)
-	{
-		int ONE = one.val; int TWO = two.val;
-		one.val = TWO; two.val = ONE;
-	}
+    private void SwapPairs_Values_Recursive_Internal(ListNode curr)
+    {
+        if (curr == null)
+        {
+            return;
+        }
 
-	#endregion
+        ListNode next = curr.next;
+
+        if (next == null)
+        {
+            return;
+        }
+
+        SwapValues(curr, next);
+        SwapPairs_Values_Recursive_Internal(next.next);
+    }
+
+    private void SwapValues(ListNode one, ListNode two)
+    {
+        int ONE = one.val; int TWO = two.val;
+        one.val = TWO; two.val = ONE;
+    }
+
+    #endregion
 }
